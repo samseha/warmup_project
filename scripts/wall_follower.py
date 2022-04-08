5a13f652d043dd5d82120500f6953d0167356c80
sam choi <samseha@uchicago.edu>
2022-04-08 01:16:14 +0000

    wall

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -5,8 +5,8 @@ from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
 #How close we will get to wall
-distance = 0.7
-k = 0.4
+distance = 1
+k = 0.2
 
 class WallFollower(object):
     """Navigates to wall and drive alongside wall"""
@@ -25,49 +25,67 @@ class WallFollower(object):
             #check if robot is not at wall
             for dist in data.ranges:
                 #check if there is a wall nearby
-                if dist < 1.2:
+                if dist < 1.5:
                     self.at_wall = True
                     break
-        if not self.at_wall or data.ranges[0] == math.inf:
-            #not at wall go to wall
-            speed = 0.3
-            return speed
-        else:
-            #at wall proceed with wall following speed
-            speed = k * (data.ranges[0] - distance)
-            if speed > 0.3:
-                return 0.3
-            return speed
+        speed = k * data.ranges[0]
+        if speed > 0.3:
+            return 0.3
+        return speed
 
     def get_angle(self, data):
+        # if not self.at_wall:
+        #     #go straight until wall
+        #     return 0
+        # #find which way to turn using trigonometry
+        # min_dist = math.inf
+        # min_angle = 0
+        # turn_angle = 0
+        # for i in range(len(data.ranges)):
+        #     if min_dist > data.ranges[i] and data.ranges[i] != 0:
+        #         min_dist = data.ranges[i]
+        #         min_angle = i
+        # rospy.loginfo("minangle: %f", min_angle)
+        # if min_dist == math.inf:
+        #     return 0
+        # rospy.loginfo("mindist: %f, %f", min_dist, data.ranges[0])
+        # if min_angle < 180:
+        #     #wall on leftside
+        #     if abs(data.ranges[0] - min_dist) < 0.35:
+        #         return -90
+        #     turn_angle = -(90 - (min_angle + 1))
+        # else:
+        #     #wall on right side
+        #     if abs(data.ranges[0] - min_dist) < 0.35:
+        #         return 90
+        #     turn_angle = 359 - min_angle
+        
+        # return turn_angle
         if not self.at_wall:
             #go straight until wall
             return 0
         #find which way to turn using trigonometry
-        min_dist = data.ranges[0]
-        min_angle = 0
-        turn_angle = 0
-        for i in range(len(data.ranges)):
-            if min_dist > data.ranges[i]:
-                min_dist = data.ranges[i]
-                min_angle = i
-        rospy.loginfo("minangle: %f", min_angle)
-        if min_dist == math.inf:
-            return 0
-        if min_angle < 180:
-            #wall on leftside
-            turn_angle = -(90 - (min_angle + 1))
+        front_dist = data.ranges[0]
+        dist_45 = data.ranges[44]
+        dist_90 = data.ranges[89]
+        dist_135 = data.ranges[134]
+        dist_180 = data.ranges[179]
+        #will always go clockwise
+        angle_diff = dist_135 - dist_45
+        wall_ang = distance - dist_90
+
+        if angle_diff == math.inf:
+            return -dist_45
+        elif angle_diff == -1 * math.inf:
+            return dist_135
         else:
-            #wall on right side
-            turn_angle = 90 - (359 - min_angle)
-        
-        return turn_angle
+            return -(angle_diff + wall_ang)
 
     def process_scan(self, data):
         #process the scan and publish speed
         self.at_wall = False
         self.vel.linear.x = self.get_x(data)
-        self.vel.angular.z = math.radians(self.get_angle(data))
+        self.vel.angular.z = self.get_angle(data)
         rospy.loginfo("x = %f", self.vel.linear.x)
         rospy.loginfo("data = %f", data.ranges[0])
         rospy.loginfo("angle = %f", math.degrees(self.vel.angular.z))
